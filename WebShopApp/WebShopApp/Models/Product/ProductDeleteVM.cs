53f72318e15e54b8312e5a9e9779e0c4a666add1
andyEmilowa <andyowaa@gmail.com>
2024-01-11 10:20:53 +0200

    1

---
+++ WebShopApp/WebShopApp/Models/Product/ProductDeleteVM.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebShopApp.Models.Product
+{
+    public class ProductDeleteVM
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Product Name")]
+        public string ProductName { get; set; } = null!;
+
+        [Display(Name = "Brand")]
+        public int BrandId { get; set; }
+        public string BrandName { get; set; }
+
+        [Required]
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        [Display(Name = "Picture")]
+        public string Picture { get; set; }
+
+        [Range(0, 5000)]
+        [Display(Name = "Quantity")]
+        public int Quantity { get; set; }
+
+        [Display(Name = "Price")]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Discount")]
+        public decimal Discount { get; set; }
+
+    }
+}
